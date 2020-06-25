361c7fc54c7166d10d164bbe1347a39505fd48ba
Alex <o.babenko@i.ua>
2020-06-25 16:08:00 +0300

    Initial commit

---
+++ src/test/java/info/unproj/EshopApplicationTests.java
@@ -0,0 +1,13 @@
+package info.unproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
